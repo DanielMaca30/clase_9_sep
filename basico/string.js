74b1abd22c1adfda57b13417b057efc9b5465541
DanielMaca30 <daniel.maca@uao.edu.co>
2024-09-05 20:15:52 -0500

    tercer commit

---
+++ basico/string.js
@@ -0,0 +1,13 @@
+let string1 = "hola como estas";
+let string2 = "espero no del todo mal";
+let string3 = `${string1}, ${string2}, y te extaño desde el dia que te fuiste`
+
+console.log(string3)
+
+console.log(string3.length)
+console.log(string3.toLocaleUpperCase())
+console.log(string3.toLocaleLowerCase())
+
+let frase = "javaScript es genial"
+
+console.log()
---
+++ basico/variables.js
@@ -0,0 +1,24 @@
+let variableCambie = "calquier valor"
+const VariableEstatica = "Variable que no cambia"
+// datos primitivos
+let names = "Daniel Maca"
+let numbers = 4;
+let boolean = true;
+let nada = null;
+let indefinidos = undefined;
+//simbolo
+let simbolo = Symbol("unico");
+//objetos
+let carro = {
+    marca: "Chevrolet",
+    modelo: "2016",
+    valor: 54000000
+}
+
+//arreglos
+let verduras = ['platano', 'zanahoria', 'tomate']
+
+//funciones
+function danielMaca(){
+    
+}
